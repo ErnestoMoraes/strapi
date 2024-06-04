71e526bade3d58f0d8f3f82b76b8e9c9c4a40cc7
ErnestoMoraes <joseernestosantosmoraes@gmail.com>
2024-06-04 10:56:23 -0300

    mexirica

--- config/plugins.js
+++ config/plugins.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   'users-permissions': {
     config: {
-      jwtSecret: env('JWT_SECRET', 'your-secret-key'),
+      jwtSecret: env('JWT_SECRET', '3+5uOB3YAWmfj/D7q2PNng=='),
     },
   },
 });
