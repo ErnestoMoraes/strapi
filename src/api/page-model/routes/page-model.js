da363d4202f0b19b6b9ac6fbd7f65508614fe474
ErnestoMoraes <joseernestosantosmoraes@gmail.com>
2024-06-04 10:25:18 -0300

    ok

---
+++ src/api/page-model/routes/page-model.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * page-model router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::page-model.page-model');
